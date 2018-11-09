695bed9ddb3a465ef3cb796fbc5d1bcd612c0c16
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-11-09 12:11:21 -0600

    v1.1 - March 25, 2014
    v1.1 - March 25, 2014
    o Rewrote edit_db.php to properly set the database settings
    
    v1.0 - October 22th, 2013
    o Initial Release

---
+++ edit_db.php
@@ -0,0 +1,243 @@
+<?php
+global $modSettings;
+
+// If we have found SSI.php and we are outside of SMF, then we are running standalone.
+if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+	require_once(dirname(__FILE__) . '/SSI.php');
+elseif (!defined('SMF')) // If we are outside SMF and can't find SSI.php, then throw an error
+	die('<b>Error:</b> Cannot install - please verify you put this file in the same place as SMF\'s SSI.php.');
+db_extend('packages');
+	
+// Get current settings
+$request = $smcFunc['db_query']('', '
+	SELECT variable, value
+	FROM {db_prefix}settings
+	WHERE variable = "censor_vulgar"');
+$row = $smcFunc['db_fetch_assoc']($request);
+$modSettings['censor_vulgar'] = $row['value'];
+$smcFunc['db_free_result']($request);
+
+$request = $smcFunc['db_query']('', '
+	SELECT variable, value
+	FROM {db_prefix}settings
+	WHERE variable = "censor_proper"');
+$row = $smcFunc['db_fetch_assoc']($request);
+$modSettings['censor_proper'] = $row['value'];
+$smcFunc['db_free_result']($request);
+
+// Define all the words to censor:
+$censor_vulgar = array(
+	'[a@][rs$][s$](|ho|ram|e)',
+	'amcik',
+	'andskota',
+	'arschloch',
+	'ayir',
+	'b[i!1][t\+7]ch',
+	'bastard',
+	'boiolas',
+	'bollock',
+	'breasts',
+	'buceta',
+	'butt-pirate',
+	'c[o0]ck',
+	'cabron',
+	'cawk',
+	'cazzo',
+	'chink',
+	'chraa',
+	'chuj',
+	'cipa',
+	'clits',
+	'cum',
+	'cunt',
+	'*damn',
+	'*d4mn',
+	'd[ae](|y)go',
+	'dick',
+	'dike',
+	'dildo',
+	'*dyke',
+	'dirsa',
+	'dupa',
+	'dziwka',
+	'ejac',
+	'Ekrem',
+	'Ekto',
+	'enculer',
+	'faen',
+	'fag',
+	'fanculo',
+	'fanny',
+	'fat[a@][rs$][s$](|e)',
+	'f[uc][uc]k',
+	'feces',
+	'feg',
+	'Felcher',
+	'ficken',
+	'fitt',
+	'Flikker',
+	'foreskin',
+	'Fotze',
+	'Fu(',
+	'*fuck',
+	'fuk',
+	'futkretzn',
+	'fux0r',
+	'frig',
+	'frigin',
+	'friggin',
+	'gay',
+	'gaydar',
+	'gook',
+	'guiena',
+	'h0r',
+	'hax0r',
+	'h4xor',
+	'h4x0r',
+	'hell',
+	'helvete',
+	'hoer',
+	'honkey',
+	'hore',
+	'Huevon',
+	'hui',
+	'injun',
+	'jackass',
+	'jism',
+	'jizz',
+	'kanker',
+	'kawk',
+	'kike',
+	'klootzak',
+	'knulle',
+	'kuk',
+	'kuksuger',
+	'Kurac',
+	'kurwa',
+	'kusi',
+	'kyrpa',
+	'l3i+ch',
+	'l3itch',
+	'l3i7ch',
+	'l3!tch',
+	'l3!+ch',
+	'lesbian',
+	'lesbo',
+	'mamhoon',
+	'masturbat',
+	'merd',
+	'mibun',
+	'monkleigh',
+	'motherfuck',
+	'mofo',
+	'mouliewop',
+	'muie',
+	'mulkku',
+	'muschi',
+	'nazi',
+	'nepesaurio',
+	'nigga',
+	'nigger',
+	'nutsack',
+	'orospu',
+	'paska',
+	'penis',
+	'perse',
+	'phuck',
+	'picka',
+	'pierdol',
+	'pillu',
+	'pimmel',
+	'pimpis',
+	'piss',
+	'pizda',
+	'poontsee',
+	'poop',
+	'porn',
+	'p0rn',
+	'pr0n',
+	'preteen',
+	'prick',
+	'pula',
+	'pule',
+	'pusse',
+	'pussy',
+	'puta',
+	'puto',
+	'qahbeh',
+	'queef',
+	'queer',
+	'qweef',
+	'rautenberg',
+	'schaffer',
+	'scheiss',
+	'schlampe',
+	'schmuck',
+	'screw',
+	'scrotum',
+	'*shit',
+	'sh!t',
+	'sharmuta',
+	'sharmute',
+	'shemale',
+	'shipal',
+	'shiz',
+	'skribz',
+	'skurwysyn',
+	'slut',
+	'smut',
+	'sphencter',
+	'spic',
+	'spierdalaj',
+	'splooge',
+	'suka',
+	'teets',
+	'b00b',
+	'teez',
+	'testicle',
+	'titt',
+	'tits',
+	'twat',
+	'vagina',
+	'viag',
+	'v1ag',
+	'v14g',
+	'vi4g',
+	'vittu',
+	'w00se',
+	'wank',
+	'wetback',
+	'whoar',
+	'whore',
+	'wichser',
+	'wop',
+	'wtf',
+	'yed',
+	'jerk',
+	'dipwad',
+	'zabourah',
+);
+$censor_proper = array();
+foreach ($censor_vulgar as $word)
+	$censor_proper[] = '*';
+$censor_vulgar = implode("\n", $censor_vulgar);
+$censor_proper = implode("\n", $censor_proper);
+$temp_censor_vulgar = str_replace($censor_vulgar . "\n", '', $modSettings['censor_vulgar']);
+$temp_censor_proper = str_replace($censor_proper . "\n", '', $modSettings['censor_proper']);
+
+// Add these words if not uninstalling:
+if (empty($context['uninstalling']))
+{
+	$temp_censor_vulgar = $censor_vulgar . "\n" . $temp_censor_vulgar;
+	$temp_censor_proper = $censor_proper . "\n" . $temp_censor_proper;
+}
+
+// Update the database with the new settings:
+updateSettings(array(
+  'censor_vulgar' => $temp_censor_vulgar,
+  'censor_proper' => $temp_censor_proper,
+));
+
+if (SMF == 'SSI')
+   echo 'Congratulations! You have successfully installed this mod!';
+?>
---
+++ readme.txt
@@ -0,0 +1,26 @@
+[hr]
+[center][color=red][size=16pt][b]WORD CENSOR LIST v1.1[/b][/size][/color]
+[url=http://www.simplemachines.org/community/index.php?action=profile;u=253913][b]By Dougiefresh[/b][/url] -> [url=http://custom.simplemachines.org/mods/index.php?mod=3796]Link to Mod[/url]
+[/center]
+[hr]
+
+[color=blue][b][size=12pt][u]Introduction[/u][/size][/b][/color]
+So, you want to run a family friendly community, without any vulgar words appearing on your site. The easiest way to prevent that is to use SMF's word censor feature, but you have an empty list of words and don't want to spend an hour filling in every naughty word you know and some you don't.
+
+Word Censor List will help you by adding a list of some commonly censored words and some uncommon ones to your forum.
+
+[color=blue][b][size=12pt][u]Compatibility Notes[/u][/size][/b][/color]
+This mod was tested on SMF 2.0.5, but should work on earlier versions of SMF 2.0.x.  SMF 1.x is not and will not be supported.
+
+[color=blue][b][size=12pt][u]Changelog[/u][/size][/b][/color]
+[quote]
+[b][u]v1.1 - March 25, 2014[/u][/b]
+o Rewrote edit_db.php to properly set the database settings
+
+[b][u]v1.0 - October 22th, 2013[/u][/b]
+o Initial Release
+[/quote]
+
+[hr]
+[url=http://creativecommons.org/licenses/by/3.0][img]http://i.creativecommons.org/l/by/3.0/80x15.png[/img][/url]
+This work is licensed under a [url=http://creativecommons.org/licenses/by/3.0]Creative Commons Attribution 3.0 Unported License[/url]
